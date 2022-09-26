427315ab6f88a25f30f8bc9b6e4ef59430bb5b3c
Serhii-Golik <golik.cergey04@gmail.com>
2022-09-26 23:26:22 +0300

    lab2
    lab2

---
+++ User/Program.cs
@@ -0,0 +1,9 @@
+﻿internal static class Program
+{
+    static void Main(string[] args)
+    {
+        User user = new User("SergGol2004", "Serg", "Gol", DateTime.Now.Year - 2004);
+        
+        Console.WriteLine(user);
+    }
+}
